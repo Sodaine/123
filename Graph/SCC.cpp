0e0ee4035c9598199e2e1843fba3a654e779be96
Sodaine <31346926+Sodaine@users.noreply.github.com>
2019-07-30 13:53:52 +0800

    Update SCC.cpp

--- Graph/SCC.cpp
+++ Graph/SCC.cpp
@@ -3,26 +3,18 @@ vi e[N],g[N],order;
 //e[u].pb(v),g[v].pb(u):e为原图，g为逆图
 void topo(int u) {
 	vis[u] = 1;
-	for(int v:g[u]) {
-		if(!vis[v]) topo(v);
-	}
+	for(int v:g[u]) if(!vis[v]) topo(v);
 	order.pb(u);
 }
 void scc(int u,int c) {
 	vis[u] = 0,id[u] = c;
-	for(int v:e[u]) {
-		if(vis[v]) scc(v,c);
-	}
+	for(int v:e[u]) if(vis[v]) scc(v,c);
 }
 void solve(int n)
 {
 	memset(vis,0,sizeof(vis[0])*n);
-	rep(i,0,n) {
-		if(!vis[i]) topo(i);
-	}
+	rep(i,0,n) if(!vis[i]) topo(i);
 	reverse(order.begin(),order.end());
   	int _=0;
-  	for(int u:order) {
-		if(vis[u]) scc(u,_++);
-	}
+  	for(int u:order) if(vis[u]) scc(u,_++);
 }
