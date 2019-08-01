1f7de7e0b89aadb8ece9facd5b24a06f9f59a1e7
Sodaine <31346926+Sodaine@users.noreply.github.com>
2019-08-01 21:58:38 +0800

    Create kmp.cpp

---
+++ string/kmp.cpp
@@ -0,0 +1,17 @@
+void getnxt() {
+    nxt[0] = -1,nxt[1] = 0;
+    int len = s.length();
+    for(int i = 1,j = 0;i < len;i++) {
+        while(j >= 0 && s[i] != s[j]) j = nxt[j];
+        nxt[i+1] = ++j;
+    }
+}
+
+for(int i = 0,j = 0;i < tlen;i++) {
+    while(j >= 0 && t[i] != s[j]) j = nxt[j];
+    if(++j == slen) cnt++;//t中找到一个s
+}
+rep(i,2,n+1) {
+		if(nxt[i] && (i) % (i-nxt[i]) == 0) 
+            //长度为i的前缀的循环节大小为i-nxt[i]
+}
