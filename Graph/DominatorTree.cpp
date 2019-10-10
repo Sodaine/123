62bd9d5b8d03a866afc35c30ec622d503050c0ad
Sodaine <31346926+Sodaine@users.noreply.github.com>
2019-10-10 13:01:51 +0800

    Update DominatorTree.cpp

--- Graph/DominatorTree.cpp
+++ Graph/DominatorTree.cpp
@@ -1,12 +1,12 @@
 /**
  * 最近必经点 (idom): 节点 y 的必经点集合 dom(y) 中 dfn 值最大的点 x 是距离 y 最近的必经点
  *                   称为 y 的最近必经点, 最近必经点是唯一的, 记 x=idom(y)
- * 半必经点 (semi) : 在搜索树 T 上点 y 的祖先中，通过非树枝边可以到达 y 的深度最小的祖先 x ，
+ * 半必经点 (semi): 在搜索树 T 上点 y 的祖先中，通过非树枝边可以到达 y 的深度最小的祖先 x ，
  *                 称为 y 的半必经点。半必经点也是唯一的，记 x=semi(y)。
  *
  * 1. 设有向图 G=(V,E)， (G,r) 是一个 Flow Graph，则称 (G,r) 的子图
- *    D=(V, { (idom(i),i) | i∈V,i≠r }, r) 为 (G,r) 的一棵 Dominator Tree。
- * 2.  (G,r) 的 Dominator Tree 是一棵有向有根树，从 r 出发可以到达 G 中的所有点，
+ *    D=(V, { (idom(i),i) | i ∈ V,i ≠ r }, r) 为 (G,r) 的一棵 Dominator Tree 。
+ * 2. (G,r) 的 Dominator Tree 是一棵有向有根树，从 r 出发可以到达 G 中的所有点，
  *    并且树上的每条边 (u,v) 都满足： u=idom(v) ，即父节点是子节点的最近必经点。
  * 3. x=idom(y) ，当且仅当有向边 (x,y) 是 Dominator Tree 中的一条树枝边。
  * 4. x dom y ，当且仅当在 Dominator Tree 中存在一条从 x 到 y 的路径。
@@ -21,7 +21,7 @@
  *    考虑 1 的每个儿子 v , 同一颗子树的节点对都是非法的.
  *
  * succ 是原图,  pred 是边反向后的图,  dom 是 Dominator Tree
- *  dom 记录的是 dfs 序构成的树,  G 中节点 u 在 dom 树上的标号是 dfn[u]
+ * dom 记录的是 dfs 序构成的树,  G 中节点 u 在 dom 树上的标号是 dfn[u]
  * 相反 dom 中节点 u 在原图 G 中的标号是 pt[u]
  * 调用 build 得到以 s 为根的 Dominator Tree
  **/
